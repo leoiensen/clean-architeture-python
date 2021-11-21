9c669422380fefacd6ffbf339180991d8323db24
Edson Leonardo Iensen <leoiensen30@gmail.com>
2021-11-21 10:33:36 -0300

    feat/Creating UserRepository with insert_user and test it

--- src/infra/config/db_config.py
+++ src/infra/config/db_config.py
@@ -10,7 +10,7 @@ class DBConnectionHandler:
         self.session = None
 
     def get_engine(self):
-        """Return Connection Engine
+        """Return connection Engine
         :parram - None
         :return - engine connection to Database
         """
@@ -24,4 +24,4 @@ class DBConnectionHandler:
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        self.session.close()  # pylint: disable=no-member
+        self.session.close()
